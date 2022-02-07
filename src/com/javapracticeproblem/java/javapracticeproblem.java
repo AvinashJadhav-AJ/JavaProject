1c19f5fcd1f8f83d5178683043c3a7f34492766f
Avinash <aj912873@gmail.com>
2022-02-07 09:48:49 +0530

    Added: Odd Even Program

--- src/com/javapracticeproblem/java/javapracticeproblem.java
+++ src/com/javapracticeproblem/java/javapracticeproblem.java
@@ -1,9 +1,18 @@
 package com.javapracticeproblem.java;
 
+import java.util.Scanner;
 
 public class javapracticeproblem {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Java Project");
+		Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + "Number is even");
+        else
+            System.out.println(num + "Number is odd");
 	}
 
 }
