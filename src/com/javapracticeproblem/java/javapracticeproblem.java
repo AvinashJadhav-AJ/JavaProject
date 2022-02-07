93ae5e9d88b3e12666e32213a72f594f1a6cc7e7
Avinash <aj912873@gmail.com>
2022-02-07 10:02:46 +0530

    Added: Scanner Object Closed

--- src/com/javapracticeproblem/java/javapracticeproblem.java
+++ src/com/javapracticeproblem/java/javapracticeproblem.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class javapracticeproblem {
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
+		Scanner Number = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int num = reader.nextInt();
+        int num = Number.nextInt();
 
         if(num % 2 == 0)
             System.out.println(num + "Number is even");
         else
             System.out.println(num + "Number is odd");
+        Number.close();
 	}
 
 }
